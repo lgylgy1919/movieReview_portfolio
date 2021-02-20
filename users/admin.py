8b9b117f3c4d1180f245d3238943e66bc44513d5
jaeyoon <lgylgy1919@gmail.com>
2021-02-20 23:34:10 +0900

    substituing user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from . import models
 
-# Register your models here.
+"""
+admin.site.register(User, UserAdmin)
+"""
+
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
