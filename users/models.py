47354add94bb7a0754a2e073e83f2cad9868277a
jaeyoon <lgylgy1919@gmail.com>
2021-02-21 23:39:05 +0900

    substituting user model(2)

--- users/models.py
+++ users/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 # User model
 class User(AbstractUser):
-    bio = models.CharField(max_length=100, default="Add biography")
+
+    """ Custom User Model """
+
+    name = models.CharField(max_length=30, null=True)
+    bio = models.TextField(max_length=100, default="Add biography")
+    age = models.CharField(max_length=3, null=True)
+    email = models.CharField(max_length=100, default="Add your email")
+    password = models.CharField(max_length=20, default="Add your password")
+    reviews = models.CharField(max_length=100, default="Reviews what you wrote")
