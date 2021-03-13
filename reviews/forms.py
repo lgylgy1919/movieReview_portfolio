2c3aca02a542b0c94bf725e193943cdc8debe3d6
jaeyoon <lgylgy1919@gmail.com>
2021-03-13 23:45:15 +0900

    review create (1)

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,6 +3,13 @@ from . import models
 
 
 class CreateReviewForm(forms.ModelForm):
+
+    story = forms.IntegerField(max_value=5, min_value=1)
+    ost = forms.IntegerField(max_value=5, min_value=1)
+    visual = forms.IntegerField(max_value=5, min_value=1)
+    director = forms.IntegerField(max_value=5, min_value=1)
+    actinv = forms.IntegerField(max_value=5, min_value=1)
+
     class Meta:
         model = models.Review
         fields = (
